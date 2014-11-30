09e918887393a9e78d7c4de305d1f3d26ce1a3c2
ifapmzadu6 <ifapmzadu6@gmail.com>
2014-12-01 01:45:30 +0900

    Initial Commit

---
+++ TermRankingMethod/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TermRankingMethod
+//
+//  Created by Keisuke Karijuku on 2014/12/01.
+//  Copyright (c) 2014年 Keisuke Karijuku. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
